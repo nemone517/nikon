3180ca529f4b1aa37fa9c0ae4435b50b0d917f4b
nemone517 <94097423+nemone517@users.noreply.github.com>
2022-02-28 14:50:24 +0900

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,43 @@
+console.clear();
+
+
+function SwiperBox1__init() {
+    const swiper = new Swiper(".swiper-box-1 .swiper", {
+      loop: true,
+      pagination: {
+        el: ".swiper-box-1 .swiper-pagination"
+      },
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-box-1 .swiper-button-next",
+        prevEl: ".swiper-box-1 .swiper-button-prev"
+      },
+      on: {
+        slideChangeTransitionStart: function () {
+          $('.swiper-box-1 [data-aos]').hide();
+          $('.swiper-box-1 [data-aos]').removeClass('aos-init').removeClass('aos-animate');
+        },
+        slideChangeTransitionEnd: function () {
+          $('.swiper-box-1 [data-aos]').show();
+          AOS.init();
+        }
+      }
+    });
+  }
+
+  
+function SwiperBox3__init() {
+    const swiper = new Swiper('.swiper-box-3 .swiper', {
+      slidesPerView:1.3,
+      spaceBetween:30,
+      loop: true,
+      centeredSlides: true,
+     
+      pagination: {
+        el: ".swiper-box-3 .swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  SwiperBox3__init();
